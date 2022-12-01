a0d5699724ac10aa22b92844c030b11036a51d46
aaa1115910 <aaa1115910@gmail.com>
2022-12-01 21:25:45 +0800

    保持屏幕常亮

--- app/src/main/kotlin/dev/aaa1115910/bv/PlayerActivity.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/PlayerActivity.kt
@@ -12,6 +12,7 @@ import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
 import android.view.KeyEvent.KEYCODE_DPAD_UP
 import android.view.KeyEvent.KEYCODE_MENU
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -49,6 +50,7 @@ class PlayerActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         //val trackSelector = DefaultTrackSelector(this, AdaptiveTrackSelection.Factory())
         val player = ExoPlayer
             .Builder(this)
@@ -77,6 +79,10 @@ class PlayerActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    override fun onDestroy() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
