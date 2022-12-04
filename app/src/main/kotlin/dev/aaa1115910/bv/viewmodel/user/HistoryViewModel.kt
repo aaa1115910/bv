dcc663c3d7713140c3ea45061f863fd48f89121f
aaa1115910 <aaa1115910@gmail.com>
2022-12-04 16:19:11 +0800

    去除历史记录的错误提示

--- app/src/main/kotlin/dev/aaa1115910/bv/viewmodel/user/HistoryViewModel.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/viewmodel/user/HistoryViewModel.kt
@@ -1,9 +1,6 @@
 package dev.aaa1115910.bv.viewmodel.user
 
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.aaa1115910.biliapi.BiliApi
@@ -23,16 +20,7 @@ class HistoryViewModel : ViewModel() {
     private var max = 0L
     private var viewAt = 0L
 
-    var showError by mutableStateOf(false)
-    var error: Throwable? by mutableStateOf(null)
-
     private var updating = false
-
-    fun clearData() {
-        max = 0L
-        viewAt = 0L
-        histories.clear()
-    }
 
     fun update() {
         viewModelScope.launch(Dispatchers.Default) {
@@ -70,10 +58,6 @@ class HistoryViewModel : ViewModel() {
             logger.info { "Update histories success" }
         }.onFailure {
             logger.info { "Update histories failed: ${it.stackTraceToString()}" }
-            showError = true
-            error = it
-        }.onSuccess {
-            showError = false
         }
         updating = false
     }
