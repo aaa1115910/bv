f2edc2d49dce3c9f89f05fb74860803e5f486156
AoEiuV020 <aoeiuv020@gmail.com>
2024-06-15 00:16:49 +0800

    修复没有谷歌配置文件无法运行的问题
    一方面判断配置文件存在才启用谷歌插件，
    一方面所有firebase相关封装起来，初始化失败就不调用其他方法，

--- app/src/main/kotlin/dev/aaa1115910/bv/util/KLoggerExtends.kt
+++ app/src/main/kotlin/dev/aaa1115910/bv/util/KLoggerExtends.kt
@@ -1,36 +1,38 @@
 package dev.aaa1115910.bv.util
 
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
 import dev.aaa1115910.bv.BuildConfig
 import io.github.oshai.kotlinlogging.KLogger
 
 fun KLogger.fInfo(msg: () -> Any?) {
     info(msg)
-    Firebase.crashlytics.log("[Info] ${msg.toStringSafe()}")
+    firebaseLog("[Info] ${msg.toStringSafe()}")
 }
 
 fun KLogger.fWarn(msg: () -> Any?) {
     warn(msg)
-    Firebase.crashlytics.log("[Warn] ${msg.toStringSafe()}")
+    firebaseLog("[Warn] ${msg.toStringSafe()}")
 }
 
 fun KLogger.fDebug(msg: () -> Any?) {
     if (BuildConfig.DEBUG) {
         info(msg)
-        Firebase.crashlytics.log("[Debug] ${msg.toStringSafe()}")
+        firebaseLog("[Debug] ${msg.toStringSafe()}")
     }
 }
 
 fun KLogger.fError(msg: () -> Any?) {
     error(msg)
-    Firebase.crashlytics.log("[Error] ${msg.toStringSafe()}")
+    firebaseLog("[Error] ${msg.toStringSafe()}")
 }
 
 fun KLogger.fException(throwable: Throwable, msg: () -> Any?) {
     warn { "$msg: ${throwable.stackTraceToString()}" }
-    Firebase.crashlytics.log("[Exception] ${msg.toStringSafe()}: ${throwable.localizedMessage}")
-    Firebase.crashlytics.recordException(throwable)
+    firebaseLog("[Exception] ${msg.toStringSafe()}: ${throwable.localizedMessage}")
+    FirebaseUtil.recordException(throwable)
+}
+
+private fun firebaseLog(msg: String) {
+    FirebaseUtil.log(msg)
 }
 
 @Suppress("NOTHING_TO_INLINE")
