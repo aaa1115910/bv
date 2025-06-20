e3610cec6d4493e5af5229ffac9474b7e3240191
fantasytyx <fantacytyx@foxmail.com>
2025-06-20 22:45:20 +0800

    优化视频时长显示，时长>=1小时时，显示小时

--- utils/src/main/kotlin/dev/aaa1115910/bv/util/LongExtends.kt
+++ utils/src/main/kotlin/dev/aaa1115910/bv/util/LongExtends.kt
@@ -6,13 +6,14 @@ fun Long.formatMinSec(): String {
     return if (this < 0L) {
         "00:00"
     } else {
-        String.format(
-            "%02d:%02d",
-            TimeUnit.MILLISECONDS.toMinutes(this),
-            TimeUnit.MILLISECONDS.toSeconds(this) -
-                    TimeUnit.MINUTES.toSeconds(
-                        TimeUnit.MILLISECONDS.toMinutes(this)
-                    )
-        )
+        val hours = TimeUnit.MILLISECONDS.toHours(this)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(this) - TimeUnit.HOURS.toMinutes(hours)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(this) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(this))
+        
+        if (hours > 0) {
+            String.format("%02d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            String.format("%02d:%02d", minutes, seconds)
+        }
     }
 }
